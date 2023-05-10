74ad0ca373e2715b1923689920e7266fc3ea7f00
Vikas-O7 <vikas.ohlan1997@gmail.com>
2023-05-11 00:27:23 +0530

    changes

--- server.mjs
+++ server.mjs
@@ -7,10 +7,10 @@ const port = 3000;
 app.get("/", (req, res) => {
   const { name, content } = req.query; // extract the name and content from query parameters
   var myHeaders = new Headers();
-  /*myHeaders.append(
+  myHeaders.append(
     "authorization",
     "Bearer eyJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2ODM4MzExNjQsImlzcyI6Ijk3NUQwMTcyNUQ1QTZCMUMwQTQ5NUVGNUBBZG9iZU9yZyIsInN1YiI6IjQyOUUyODg0NjQ0QjkwMEEwQTQ5NUMzREB0ZWNoYWNjdC5hZG9iZS5jb20iLCJodHRwczovL2ltcy1uYTEuYWRvYmVsb2dpbi5jb20vcy9lbnRfbWFya2V0aW5nX3NkayI6dHJ1ZSwiYXVkIjoiaHR0cHM6Ly9pbXMtbmExLmFkb2JlbG9naW4uY29tL2MvZjY0MjllMzc3YzlmNGI2ZDlhOTBkYzMyYWEzYWY5MzkifQ.yPr6w3zLXHZg-u_Sm2cMPFYbn-HilpC1VAHzUlu_rnSMWTM3PGwHGvWtEn6DRJwNfEfgG3mWeLyjND6ZbcYUU9LVNW458-t-hyvMqUI9lYmcGo2ub4gXOFz7oNRFN2YUd7P1VFb06j5mmsFQpczkzQuZGl-aKQJX0L6Gx1r3FoGFrVCs5KjrP-KTfSha6nTGLOWjHEKunZf4ndB5BGK-hVB709n1MKwPu3DIxmKEhLKBgM6P0LmAdZWgqc7HwVl4euPYC74YJ9yDc05pRD33u96gFzun5mToZ9uSp9EXXY7lS15B7pGKbeutXfYqoP1MzgvlusIrTbg9SFbihz2gEg"
-  ); */
+  ); 
 
   myHeaders.append("cache-control", "no-cache");
   myHeaders.append("content-type", "application/vnd.adobe.target.v2+json");
